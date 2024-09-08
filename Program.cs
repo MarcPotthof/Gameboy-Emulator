6fd3e7ed6677402a38256eedc11505474f13ee88
Marc <PotthofMarc@gmail.com>
2024-09-08 17:56:40 +0200

    CPU Klasse zu Struct umgeändert, einen teil der 8-bit load opcodes hinzugefügt

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using Microsoft.Win32;
 using System.Runtime.CompilerServices;
 
 internal class Program
@@ -24,6 +25,11 @@ namespace GBEmulator
         public FlagRegister f;
         public byte h;
         public byte l;
+
+        public ushort af { get { return (UInt16)((a << 8) | f.As_byte()); } set {a = (byte)((value & 0xff00) >> 8);f.From_byte((byte)(value & 0xff));} }
+        public ushort bc { get { return (UInt16)((b << 8) | c); } set { b = (byte)((value & 0xff00) >> 8);c = (byte)(value & 0xff);} }
+        public ushort de { get { return (UInt16)((d << 8) | e); } set { d = (byte)((value & 0xff00) >> 8);e = (byte)(value & 0xff);} }
+        public ushort hl { get { return (UInt16)((h << 8) | l); } set { h = (byte)((value & 0xff00) >> 8); l = (byte)(value & 0xff); } }
     }
     public struct FlagRegister
     {
@@ -94,22 +100,133 @@ namespace GBEmulator
             register.l = (byte)(value & 0xff);
         }
     }
-    public class CPU
+    public struct CPU
     {
         public Register registers;
 
+        //program counter
         public ushort pc = 0x100;
+        //program memory
         public byte[] memory = new byte[0xFFFF];
-
+        //stack pointer
         public ushort sp = 0xFFFE;
+
+        public CPU() { }
     }
     public static class Instructions
     {
-        public static ushort ExecuteInstruction(this CPU CPU, ushort pc)
+        public static byte ReadAt(this CPU cpu, ushort index)
         {
-            switch (CPU.memory[pc])
+            return cpu.memory[index]; 
+        }
+        public static byte ReadByte(this CPU cpu, bool readNext = false)
+        {
+            if (readNext) cpu.pc++;
+            return cpu.memory[cpu.pc++];
+        }
+        public static ushort ReadShortAt(this CPU cpu, ushort index)
+        { 
+            return (ushort)((cpu.memory[index] << 8 | cpu.memory[index + 1]));
+        }
+        public static ushort ReadUshort(this CPU cpu, bool readNext = false)
+        {
+            if (readNext) cpu.pc++;
+            return (ushort)((cpu.memory[cpu.pc++] << 8 | cpu.memory[cpu.pc++]));
+        }
+
+        public static void WriteAt(this CPU cpu, ushort index, byte value)
+        {
+
+        }
+
+        public static void ExecuteInstruction(this CPU cpu)
+        {
+            switch (cpu.memory[cpu.pc]) 
             {
-                default: Console.WriteLine("undefined or empty opcode"); return 0x0000;
+                //Instruction list:
+                //http://marc.rawer.de/Gameboy/Docs/GBCPUman.pdf
+
+                //8-Bit Loads
+
+                //LD nn,n
+                case 0x06: cpu.registers.b = cpu.ReadByte(true); cpu.pc++; break;
+                case 0x0E: cpu.registers.c = cpu.ReadByte(true); cpu.pc++; break;
+                case 0x16: cpu.registers.d = cpu.ReadByte(true); cpu.pc++; break;
+                case 0x1E: cpu.registers.e = cpu.ReadByte(true); cpu.pc++; break;
+                case 0x26: cpu.registers.h = cpu.ReadByte(true); cpu.pc++; break;
+                case 0x2E: cpu.registers.l = cpu.ReadByte(true); cpu.pc++; break;
+                //LD r1,r2
+                case 0x7F: cpu.registers.a = cpu.registers.a; cpu.pc++; break;
+                case 0x78: cpu.registers.a = cpu.registers.b; cpu.pc++; break;
+                case 0x79: cpu.registers.a = cpu.registers.c; cpu.pc++; break;
+                case 0x7A: cpu.registers.a = cpu.registers.d; cpu.pc++; break;
+                case 0x7B: cpu.registers.a = cpu.registers.e; cpu.pc++; break;
+                case 0x7C: cpu.registers.a = cpu.registers.h; cpu.pc++; break;
+                case 0x7D: cpu.registers.a = cpu.registers.l; cpu.pc++; break;
+                case 0x7E: cpu.registers.a = cpu.ReadAt(cpu.registers.hl); cpu.pc++; break;
+
+                case 0x40: cpu.registers.b = cpu.registers.b; cpu.pc++; break;
+                case 0x41: cpu.registers.b = cpu.registers.c; cpu.pc++; break;
+                case 0x42: cpu.registers.b = cpu.registers.d; cpu.pc++; break;
+                case 0x43: cpu.registers.b = cpu.registers.e; cpu.pc++; break;
+                case 0x44: cpu.registers.b = cpu.registers.h; cpu.pc++; break;
+                case 0x45: cpu.registers.b = cpu.registers.l; cpu.pc++; break;
+                case 0x46: cpu.registers.b = cpu.ReadAt(cpu.registers.hl); cpu.pc++; break;
+
+                case 0x48: cpu.registers.c = cpu.registers.b; cpu.pc++; break;
+                case 0x49: cpu.registers.c = cpu.registers.c; cpu.pc++; break;
+                case 0x4A: cpu.registers.c = cpu.registers.d; cpu.pc++; break;
+                case 0x4B: cpu.registers.c = cpu.registers.e; cpu.pc++; break;
+                case 0x4C: cpu.registers.c = cpu.registers.h; cpu.pc++; break;
+                case 0x4D: cpu.registers.c = cpu.registers.l; cpu.pc++; break;
+                case 0x4E: cpu.registers.c = cpu.ReadAt(cpu.registers.hl); cpu.pc++; break;
+
+                case 0x50: cpu.registers.d = cpu.registers.b; cpu.pc++; break;
+                case 0x51: cpu.registers.d = cpu.registers.c; cpu.pc++; break;
+                case 0x52: cpu.registers.d = cpu.registers.d; cpu.pc++; break;
+                case 0x53: cpu.registers.d = cpu.registers.e; cpu.pc++; break;
+                case 0x54: cpu.registers.d = cpu.registers.h; cpu.pc++; break;
+                case 0x55: cpu.registers.d = cpu.registers.l; cpu.pc++; break;
+                case 0x56: cpu.registers.d = cpu.ReadAt(cpu.registers.hl); cpu.pc++; break;
+
+                case 0x58: cpu.registers.e = cpu.registers.b; cpu.pc++; break;
+                case 0x59: cpu.registers.e = cpu.registers.c; cpu.pc++; break;
+                case 0x5A: cpu.registers.e = cpu.registers.d; cpu.pc++; break;
+                case 0x5B: cpu.registers.e = cpu.registers.e; cpu.pc++; break;
+                case 0x5C: cpu.registers.e = cpu.registers.h; cpu.pc++; break;
+                case 0x5D: cpu.registers.e = cpu.registers.l; cpu.pc++; break;
+                case 0x5E: cpu.registers.e = cpu.ReadAt(cpu.registers.hl); cpu.pc++; break;
+
+                case 0x60: cpu.registers.h = cpu.registers.b; cpu.pc++; break;
+                case 0x61: cpu.registers.h = cpu.registers.c; cpu.pc++; break;
+                case 0x62: cpu.registers.h = cpu.registers.d; cpu.pc++; break;
+                case 0x63: cpu.registers.h = cpu.registers.e; cpu.pc++; break;
+                case 0x64: cpu.registers.h = cpu.registers.h; cpu.pc++; break;
+                case 0x65: cpu.registers.h = cpu.registers.l; cpu.pc++; break;
+                case 0x66: cpu.registers.h = cpu.ReadAt(cpu.registers.hl); cpu.pc++; break;
+
+                case 0x68: cpu.registers.l = cpu.registers.b; cpu.pc++; break;
+                case 0x69: cpu.registers.l = cpu.registers.c; cpu.pc++; break;
+                case 0x6A: cpu.registers.l = cpu.registers.d; cpu.pc++; break;
+                case 0x6B: cpu.registers.l = cpu.registers.e; cpu.pc++; break;
+                case 0x6C: cpu.registers.l = cpu.registers.h; cpu.pc++; break;
+                case 0x6D: cpu.registers.l = cpu.registers.l; cpu.pc++; break;
+                case 0x6E: cpu.registers.l = cpu.ReadAt(cpu.registers.hl); cpu.pc++; break;
+
+                case 0x70: cpu.WriteAt(cpu.registers.hl, cpu.registers.b); cpu.pc++; break;
+                case 0x71: cpu.WriteAt(cpu.registers.hl, cpu.registers.c); cpu.pc++; break;
+                case 0x72: cpu.WriteAt(cpu.registers.hl, cpu.registers.d); cpu.pc++; break;
+                case 0x73: cpu.WriteAt(cpu.registers.hl, cpu.registers.e); cpu.pc++; break;
+                case 0x74: cpu.WriteAt(cpu.registers.hl, cpu.registers.h); cpu.pc++; break;
+                case 0x75: cpu.WriteAt(cpu.registers.hl, cpu.registers.l); cpu.pc++; break;
+
+                case 0x36: cpu.registers.hl = cpu.ReadByte(true); break;
+
+                //LD A,n
+                case 0x0A: cpu.registers.a = (byte)(cpu.registers.bc >> 8); cpu.pc++; break;
+                case 0x1A: cpu.registers.a = (byte)
+
+                default: Console.WriteLine("undefined or empty opcode"); cpu.pc++; break;
             }
         }
 
